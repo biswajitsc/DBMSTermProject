e78f3918c74e64d4f0d408a70f410a81a5a042e4
Biswajit Paria <biswajitsc@gmail.com>
2015-04-05 10:01:19 +0530

    Rename windows

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@
 
 package main;
 
-import gui.SearchWindow;
+import gui.StatsWindow;
 
 /**
  *
@@ -19,7 +19,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        SearchWindow window = new SearchWindow();
+        StatsWindow window = new StatsWindow();
         window.main(null);
     }
     

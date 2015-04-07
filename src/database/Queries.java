6a1731f632aeb81e68957e429f5be1ad11417cb4
Biswajit Paria <biswajitsc@gmail.com>
2015-04-07 20:03:48 +0530

    matches query

--- src/database/Queries.java
+++ src/database/Queries.java
@@ -26,18 +26,17 @@ public class Queries {
      * @param type Whether ODI, Test or T-20
      * @return ResultSet of the database query containing Date, Type, Country1, Country2, Result, Winner, Margin, Location
      */
-    public static ResultSet getMatches(String cid1, String country1, String cid2, String country2,
+    public static ResultSet getMatches(String country1, String country2, String winner,
             Integer year, String location, String type)
     {
         String query = "select StartDate as Date, Type, C1.Name as Country1, C2.Name as Country2, Result, C3.Name as Winner, Margin, G.Name as Location "
                      + "from Matches, Country as C1, Country as C2, Country as C3, Ground as G "
                      + "where Matches.Team1 = C1.CID and Matches.Team2 = C2.CID and Matches.Winner = C3.CID and G.GID = Matches.Location ";
         
-        if(cid1 != null) query += "and C1.CID = "+cid1+" ";
         if(country1 != null) query += "and C1.Name = \""+country1+"\" ";
-        if(cid2 != null) query += "and C1.CID = "+cid2+" ";
         if(country2 != null) query += "and C2.Name = \""+country2+"\" ";
         if(location != null) query += "and G.Name = \""+location+"\" ";
+        if(winner != null) query += "and C3.Name = \""+winner+"\" ";
         if(type != null) query += "and Type = \""+type+"\" ";
         if(year != null) query += "and StartDate < \""+(year+1)+"-01-01\" and StartDate >= \""+year+"-01-01\" ";
         
